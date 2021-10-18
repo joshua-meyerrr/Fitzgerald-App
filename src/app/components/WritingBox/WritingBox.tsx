e90a20b1b26e5af3cb4b0ee67d08272ce476a223
joshua-meyerrr <joshua.meyer.personal@gmail.com>
2021-10-18 22:36:37 +0200

    🩹 Change WritingBox Component to ClassName

--- src/app/components/WritingBox/WritingBox.tsx
+++ src/app/components/WritingBox/WritingBox.tsx
@@ -5,15 +5,17 @@ import styles from './WritingBox.module.css';
 type WritingBoxProps = {
   InputValue: string;
   setInputChange: (InputValue: string) => void;
+  className?: string;
 };
 
 export default function WritingBox({
   InputValue,
   setInputChange,
+  className,
 }: WritingBoxProps): JSX.Element {
   return (
     <TextareaAutosize
-      className={styles.textfield}
+      className={`${styles.textfield} ${className}`}
       minRows={5}
       placeholder="Enter Text"
       value={InputValue}
